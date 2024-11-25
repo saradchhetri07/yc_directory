1ec2ce02c501b1fa6e588e20327a45b1e3452c70
saradchhetri07 <saradchhetri20690@gmail.com>
2024-11-25 13:15:03 +0545

    added search form

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,25 @@
 import Image from "next/image";
 import Navbar from "../components/Navbar";
+import SearchForm from "../components/SearchForm";
 
-export default function Home() {
-  return <div></div>;
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const query = (await searchParams).query;
+  return (
+    <>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch your StartUp, <br />
+          Connect with Entreprenuers
+        </h1>
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Pitches and get noticed in virtual Competitions
+        </p>
+        <SearchForm query={query} />
+      </section>
+    </>
+  );
 }
