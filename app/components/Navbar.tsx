daa84cf53ba851dd6e0f5ed0fd8a6cd528ea8956
saradchhetri07 <saradchhetri20690@gmail.com>
2024-11-25 10:17:18 +0545

    added heading class in utilities global css files

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,7 +1,57 @@
+import Link from "next/link";
+import Image from "next/image";
+import { auth, signOut, signIn } from "../auth";
+import Form from "next/form";
 import React from "react";
 
-const Navbar = () => {
-  return <div>Navbar</div>;
+const Navbar = async () => {
+  const session = await auth();
+
+  return (
+    <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
+      <nav className="flex justify-between items-center">
+        <Link href="/">
+          <Image src="/logo.png" alt="logo" width={144} height={30}></Image>
+        </Link>
+
+        <div className="flex items-center gap-4 text-black">
+          {session && session?.user ? (
+            <>
+              <Link href="/startup/create">
+                <span>Create</span>
+              </Link>
+              <form
+                action={async () => {
+                  "use server";
+
+                  await signOut();
+                }}
+              >
+                <button type="submit">
+                  <button>SignOut</button>
+                </button>
+                <Link href={`/user/${session?.user?.id}`}>
+                  <span>{session?.user?.name}</span>
+                </Link>
+              </form>
+            </>
+          ) : (
+            <form
+              action={async () => {
+                "use server";
+
+                await signIn("github");
+              }}
+            >
+              <button type="submit">
+                <span>Login</span>
+              </button>
+            </form>
+          )}
+        </div>
+      </nav>
+    </header>
+  );
 };
 
 export default Navbar;
